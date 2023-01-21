0ea1c90ffd59df585f86e4a69eddcc1126a333d6
lukas <mail@lukas-eisenhauer.de>
2023-01-21 20:41:46 +0100

    add basic graph

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Scatter as ChartJS } from 'chart.js/auto'
+import { Scatter }            from 'react-chartjs-2'
+
+const totalU = 12;
+const length = 10;
 
 const darkTheme = createTheme({
   palette: {
@@ -9,12 +18,61 @@ const darkTheme = createTheme({
   },
 });
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function render(x) {
+  var U = totalU * x / length;
+  if (U <= 4.9) {
+    return 0;
+  }
+  U -= 4.9;
+  const distance = U * length / totalU;
+  return Math.exp(-distance);
+}
+
+function linspace(min, max, count) {
+  const result = [];
+  for (var i = 0; i < count; i++) {
+    result.push(min + (max - min) * i / count);
+  }
+  return result;
+}
+
+const xs = linspace(0, length, 100);
+
+const intensityData = xs.map(x => {return {x: x, y: render(x)}});
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <main>
-        <Button variant="contained">Hello World</Button>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={2}>
+              <Item>Hello</Item>
+            </Grid>
+            <Grid item xs = {10}>
+              <Item><Scatter
+                options={{
+                  type: "scatter",
+                  events: [click],
+                  plugins: {legend: {labels: {filter: function(item, chart) {return item.text != "none"}}}}
+                }}
+                data={{
+                  datasets: [
+                    {type: "scatter", label: "none", data: [{x: 0, y: 0}]},
+                    {type: "line", label: "light intensity",data: intensityData},
+                ]}}/></Item>
+            </Grid>
+          </Grid>
+        </Box>
       </main>
     </ThemeProvider>
   );
